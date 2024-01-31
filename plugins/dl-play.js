bb61850b3bea9f545e810d9cffb72eac68e854f3
Jezhuz Arreola <48756069+DarkSideMoon13@users.noreply.github.com>
2024-01-31 05:08:01 -0600

    KeNy-Bot

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 	//const url = 'https://www.youtube.com/watch?v=' + videoId
 	m.react('🎧') 
 	let play = `
-	≡ *FG MUSIC*
+	≡ *KeNy MUSIC*
 ┌──────────────
 ▢ 📌 *${mssg.title()}* : ${title}
 ▢ 📆 *${mssg.uploud()}:* ${ago}
